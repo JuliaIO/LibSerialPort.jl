0f5876308219ea1290082b4737bb52cf706c3da0
Andrew Adare <andrewadare@gmail.com>
2016-02-18 19:38:15 -0700

    Play around with readline capabilities. Much slower than the nonblocking readall approach

--- test/test-high-level-api.jl
+++ test/test-high-level-api.jl
@@ -19,15 +19,51 @@ function test_nonblocking_serial_loopback(sp::SerialPort)
     for i = 1:100
         write(sp, "Test message $i\n")
         print(readall(sp))
+        i == 100 && write(sp, "done")
     end
 
-    sleep(0.1)
+    # Allow up to 100 more ms to get the remaining data
+    for i = 1:100
+        data = readall(sp)
+        print(data)
+        if contains(data, "done")
+            break
+        end
+        sleep(0.001)
+    end
 
-    # Read and print any remaining data
-    while nb_available(sp) > 0
+    println()
+    toc()
+
+    flush(sp, buffer=SP_BUF_BOTH)
+end
+
+function test_readline(sp::SerialPort)
+
+    println("\n[TEST] Read any incoming data for ~1 second...")
+    for i = 1:1000
         print(readall(sp))
+        sleep(0.001)
     end
     println()
+
+    flush(sp, buffer=SP_BUF_BOTH)
+
+    print("\n\n[TEST] Serial loopback - ")
+    println("Send 100 short messages and read whatever comes back...")
+
+    tic()
+    for i = 1:100
+        write(sp, "Test message $i\n")
+        received_message = readuntil(sp, '\n')
+        print(received_message)
+
+        # Trigger an EOF when done writing so readuntil doesn't hang forever
+        if i == 100
+            seteof(sp, true)
+        end
+    end
+    reseteof(sp)
     toc()
 
     flush(sp, buffer=SP_BUF_BOTH)
@@ -43,8 +79,50 @@ function main()
     print_port_settings(sp)
 
     test_nonblocking_serial_loopback(sp)
+    test_readline(sp)
 
     close(sp, delete=true)
 end
 
 main()
+
+# function test_readline(sp::SerialPort)
+
+#     println("\n[TEST] Read any incoming data for ~1 second...")
+#     for i = 1:1000
+#         print(readall(sp))
+#         sleep(0.001)
+#     end
+#     println()
+
+#     flush(sp, buffer=SP_BUF_BOTH)
+
+#     print("\n\n[TEST] Serial loopback - ")
+#     println("Send 100 short messages and read whatever comes back...")
+
+#     tic()
+#     for i = 1:100
+#         write(sp, "Test message $i\n")
+
+#         # Send an EOF byte
+#         if i == 100
+#             write(sp, 0x04)
+#         end
+
+#         while true
+#             b = readbytes(sp, 1)
+#             b == 0x04 && break
+#             if b != 0
+#                 c = bytestring(b)
+#                 print(c)
+#                 c == "\n" && break
+#             end
+#         end
+#         # print(readall(sp))
+#     end
+
+#     println()
+#     toc()
+
+#     flush(sp, buffer=SP_BUF_BOTH)
+# end
