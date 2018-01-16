b70242b340ca22d6b5bd627a8e686d691bacf0a6
Samuel Powell <samuelpowell@users.noreply.github.com>
2018-01-16 20:34:19 +0000

    Add windows support using AV artefact

--- deps/build.jl
+++ deps/build.jl
@@ -7,21 +7,28 @@ const src_uri = "http://sigrok.org/download/source/libserialport/libserialport-"
 
 @BinDeps.setup
 
-libserialport = library_dependency("libserialport", aliases = ["serialport", "libserialport.0"])
+libserialport = library_dependency("libserialport", aliases = ["serialport", "libserialport.0", "libserialport-0"])
 
-provides(Sources, Dict(URI(src_uri) => libserialport))
+if is_windows()
+  version == "0.1.1" || error("Only version 0.1.1 available for Windows")
+  win32_bin_uri = "https://ci.appveyor.com/api/buildjobs/1t0y955tdpvlnl27/artifacts/libserialport-0.1.1.zip"
+  win64_bin_uri = "https://ci.appveyor.com/api/buildjobs/1t0y955tdpvlnl27/artifacts/libserialport-0.1.1.zip"
+  provides(Binaries, URI(Sys.ARCH == :x86_64 ? win64_bin_uri : win32_bin_uri),libserialport, os = :Windows)
+else
+  provides(Sources, Dict(URI(src_uri) => libserialport))
 
-prefix = joinpath(BinDeps.depsdir(libserialport),"usr")
-srcdir = joinpath(BinDeps.depsdir(libserialport),"src", "libserialport-" * version)
+  prefix = joinpath(BinDeps.depsdir(libserialport),"usr")
+  srcdir = joinpath(BinDeps.depsdir(libserialport),"src", "libserialport-" * version)
 
-provides(SimpleBuild,
-    (@build_steps begin
-        GetSources(libserialport)
-        @build_steps begin
-            ChangeDirectory(srcdir)
-            `./configure --prefix=$prefix`
-            `make install`
-        end
-    end), libserialport)
+  provides(SimpleBuild,
+      (@build_steps begin
+          GetSources(libserialport)
+          @build_steps begin
+              ChangeDirectory(srcdir)
+              `./configure --prefix=$prefix`
+              `make install`
+          end
+      end), libserialport)
+end
 
 @BinDeps.install Dict(:libserialport => :libserialport)
