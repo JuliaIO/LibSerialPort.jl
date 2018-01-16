caaab0fdb51ddec361ebf3728ead1ef48e14d0f6
Samuel Powell <samuelpowell@users.noreply.github.com>
2018-01-16 21:28:18 +0000

    Provide stable download location

--- deps/build.jl
+++ deps/build.jl
@@ -4,15 +4,14 @@ using BinDeps
 
 const version = "0.1.1"
 const src_uri = "http://sigrok.org/download/source/libserialport/libserialport-" * version * ".tar.gz"
+const win32_bin_uri = "https://github.com/samuelpowell/libserialport/releases/download/" * version * "-av/libserialport-" * version * "-av-i686.zip"
+const win64_bin_uri = "https://github.com/samuelpowell/libserialport/releases/download/" * version * "-av/libserialport-" * version * "-av-x86_64.zip"
 
 @BinDeps.setup
 
 libserialport = library_dependency("libserialport", aliases = ["serialport", "libserialport.0", "libserialport-0"])
 
 if is_windows()
-  version == "0.1.1" || error("Only version 0.1.1 available for Windows")
-  win32_bin_uri = "https://ci.appveyor.com/api/buildjobs/1t0y955tdpvlnl27/artifacts/libserialport-0.1.1.zip"
-  win64_bin_uri = "https://ci.appveyor.com/api/buildjobs/1t0y955tdpvlnl27/artifacts/libserialport-0.1.1.zip"
   provides(Binaries, URI(Sys.ARCH == :x86_64 ? win64_bin_uri : win32_bin_uri),libserialport, os = :Windows)
 else
   provides(Sources, Dict(URI(src_uri) => libserialport))
