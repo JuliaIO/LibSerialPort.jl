b614a975cc6cca90100b730c85b6d1be3a51ac10
Samuel Powell <samuelpowell@users.noreply.github.com>
2019-04-05 17:59:23 +0100

    Only list ports and exit on CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,32 +12,38 @@ using LibSerialPort
 using Test
 
 
-if length(ARGS) == 0
-    port = "/dev/ttyS0"  # /dev/ttyS4 /dev/ttyUSB0
+if haskey(ENV, "CI")
+    @testset "LibSerialPort CI port listing" begin
+        list_ports()   
+    end
 else
-    port = ARGS[1]
-end
-
-baudrate = length(ARGS) >= 2 ? ARGS[2] : "115200"
-
-@testset "LibSerialPort" begin
-    @testset "Low level API" begin
-        include("test-low-level-api.jl")
-        @test test_low_level_api() == nothing
-        @test test_low_level_api(port, baudrate) == nothing
+    if length(ARGS) == 0
+        port = "/dev/ttyS0"  # /dev/ttyS4 /dev/ttyUSB0
+    else
+        port = ARGS[1]
     end
 
+    baudrate = length(ARGS) >= 2 ? ARGS[2] : "115200"
 
-    @testset "High level API" begin
-        include("test-high-level-api.jl")
-        @test test_high_level_api() == nothing
-        @test test_high_level_api(port, baudrate) == nothing
+    @testset "LibSerialPort" begin
+        @testset "Low level API" begin
+            include("test-low-level-api.jl")
+            @test test_low_level_api() == nothing
+            @test test_low_level_api(port, baudrate) == nothing
+        end
+
+
+        @testset "High level API" begin
+            include("test-high-level-api.jl")
+            @test test_high_level_api() == nothing
+            @test test_high_level_api(port, baudrate) == nothing
+        end
+
+        # console.jl runs forever, thus isn't amenable to unit testing
+        # @testset "Examples" begin
+        #     include("../examples/console.jl")
+        #     @test console() == nothing
+        #     @test console(port, baudrate) == nothing
+        # end
     end
-
-    # console.jl runs forever, thus isn't amenable to unit testing
-    # @testset "Examples" begin
-    #     include("../examples/console.jl")
-    #     @test console() == nothing
-    #     @test console(port, baudrate) == nothing
-    # end
 end
