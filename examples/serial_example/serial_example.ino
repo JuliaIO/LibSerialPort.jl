4532c9fa2093b83ab95f50ceba2f97132e3c1a3a
Andrew Adare <andrewadare@gmail.com>
2020-04-23 13:35:40 -0600

    Change serial_example to behave like a loopback device.

--- examples/serial_example/serial_example.ino
+++ examples/serial_example/serial_example.ino
@@ -7,36 +7,19 @@ unsigned long responseDelay = 0;
 bool write_adc = false;
 unsigned long writeInterval = 50;  // ms
 
-int incomingByte = 0;   // for incoming serial data
-String cmd = "";        // Usage of Arduino's String class is OK for an example
 unsigned long timeMarker = millis();
-
-void handleByte(byte b)
-{
-  if (b == '\r' || b == '\n')
-  {
-    delay(responseDelay);
-    Serial.println(cmd);
-    cmd = "";
-  }
-  else
-  {
-    cmd += char(b);
-  }
-}
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("\nGreetings from the microcontroller.");
 }
 
 void loop()
 {
   if (Serial.available() > 0)
   {
-    incomingByte = Serial.read();
-    handleByte(incomingByte);
+    delay(responseDelay);
+    Serial.write(Serial.read());
   }
   if (write_adc && millis() - timeMarker > writeInterval)
   {
